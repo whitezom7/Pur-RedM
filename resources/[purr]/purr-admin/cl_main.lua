f7ffa01bc57e80f5f83992e69dd3eb2dcb82bb1a
Taylor <contact@whitewood.dev>
2023-06-12 00:30:17 +0200

    Update cl_main.lua

--- resources/[purr]/purr-admin/cl_main.lua
+++ resources/[purr]/purr-admin/cl_main.lua
@@ -2,20 +2,17 @@ local Player = GetPlayerIndex()
 local godmode = false
 
 
-function GodMode ()
-    if godmode == false then
-        godmode = true
-        SetPlayerInvincible(Player,godmode)
-        lib.notify({title = 'God Mode',
-            description = 'God Mode is On',
-            type = 'success'})
-    else
-        godmode = false
-        SetPlayerInvincible(Player,godmode)
-        lib.notify({title = 'God Mode',
-            description = 'God Mode is Off',
-            type = 'success'})
-    end
+function GodMode()
+    godmode = not godmode
+    local modeText = godmode and "On" or "Off"
+    
+    SetPlayerInvincible(Player, godmode)
+
+    lib.notify({
+        title = 'God Mode',
+        description = 'God Mode is ' .. modeText,
+        type = 'success'
+    })
 end
 
 RegisterCommand('admin', function()
